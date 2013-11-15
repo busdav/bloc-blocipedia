dcf6394c9afb609572f4ea7212ffb82a50094533
Robert Ing <robertming@gmail.com>
2013-11-15 01:26:49 -0500

    update edit link for wikis

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,7 @@
 class Wiki < ActiveRecord::Base
   attr_accessible :body, :title, :public
   belongs_to :user
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  validates :user, presence: true #do i need this here?  shouldn't a wiki always have a user if a requirement is current_user = true?
 end
