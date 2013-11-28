2833aa951b1ded909ce60bc646c0f92485292d92
Robert Ing <robertming@gmail.com>
2013-11-27 20:35:17 -0800

    finish debug sorting, finalize sort!

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -15,7 +15,7 @@ class Wiki < ActiveRecord::Base
   scope :visible_to, lambda { |user| user.role == "premium" ? scoped : where(public: true) } #if user is premium, no filters, if not => filter by public == true
   scope :public, where(public: true) #make Wiki.public possible
   
-  default_scope order('created_at DESC')
+  #default_scope order('created_at DESC')
 
   has_many :collaborators
 end
