c6d13c3a7d353cc7095b9faa8f7542ee44bae739
Robert Ing <robertming@gmail.com>
2013-11-27 19:07:10 -0800

    debug sort for timothybarnes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,13 @@ helper_method :sort_column, :sort_direction
 
   def show
     @user = User.find(params[:id])
-    @wikis = @user.wikis.reorder(sort_column + " " + sort_direction)
+    @wikis = @user.wikis
   end
 
   def index
     @user = User.find(params[:id])
   end
 
-  private
-  
-  def sort_column
-    Wiki.column_names.include?(params[:sort]) ? params[:sort] : "title"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
+ 
 
 end
